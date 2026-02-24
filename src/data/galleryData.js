fa8e8fc3c1c0027f7bdde2d661e864f04981f4f0
Houmahani Kane <houm.kane@gmail.com>
2026-02-24 10:28:25 +0100

    ✨ Swaps gallery assets to still-life images

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -1,17 +1,15 @@
-import NASA01 from '@/assets/nasa-01.webp'
-import NASA02 from '@/assets/nasa-02.webp'
-import NASA03 from '@/assets/nasa-03.webp'
-import NASA04 from '@/assets/nasa-04.webp'
-import NASA05 from '@/assets/nasa-05.webp'
-import NASA06 from '@/assets/nasa-06.webp'
+import STILL_LIFE01 from '@/assets/still-life/still-life-01.webp'
+import STILL_LIFE02 from '@/assets/still-life/still-life-02.webp'
+import STILL_LIFE03 from '@/assets/still-life/still-life-03.webp'
+import STILL_LIFE04 from '@/assets/still-life/still-life-04.webp'
+import STILL_LIFE05 from '@/assets/still-life/still-life-05.webp'
 
 const galleryPlaneData = [
-  { color: '#ffffff', textureSrc: NASA01, position: { x: -1.6, y: 0 } },
-  { color: '#ffd166', textureSrc: NASA02, position: { x: 1.2, y: 0 } },
-  { color: '#ef476f', textureSrc: NASA03, position: { x: -0.9, y: 0 } },
-  { color: '#06d6a0', textureSrc: NASA04, position: { x: 1.8, y: 0 } },
-  { color: '#118ab2', textureSrc: NASA05, position: { x: -1.3, y: 0 } },
-  { color: '#8ecae6', textureSrc: NASA06, position: { x: 0.9, y: 0 } },
+  { color: '#ffffff', textureSrc: STILL_LIFE01, position: { x: -1.6, y: 0 } },
+  { color: '#ffd166', textureSrc: STILL_LIFE02, position: { x: 1.2, y: 0 } },
+  { color: '#ef476f', textureSrc: STILL_LIFE03, position: { x: -0.9, y: 0 } },
+  { color: '#06d6a0', textureSrc: STILL_LIFE04, position: { x: 1.8, y: 0 } },
+  { color: '#118ab2', textureSrc: STILL_LIFE05, position: { x: -1.3, y: 0 } },
 ]
 
 export { galleryPlaneData }
